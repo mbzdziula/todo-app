f26d0e3f00b1651ba21ee0338aae9f0534a09f82
mbzdziula <mbzdziula@gmail.com>
2021-01-07 09:02:36 +0100

    add progress

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,64 @@
 import { Paper } from '@material-ui/core';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './todo-list/TodoList';
+import { fetchTodos } from '../redux/actions/todoActions';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-function Todo() {
-  return (
-    <>
-      <TodoForm />
-      <Paper>
-        <TodoList />
-      </Paper>
-    </>
-  );
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'block',
+    marginTop: '5rem',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    textAlign: 'center',
+  },
+}));
+
+function Todo(props) {
+  const classes = useStyles();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async () => {
+      await props.fetchTodos();
+      setLoading(false);
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className={classes.root}>
+        <CircularProgress size="7rem" />
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <TodoForm />
+        <Paper>
+          <TodoList />
+        </Paper>
+      </>
+    );
+  }
 }
 
-export default Todo;
+TodoList.propTypes = {
+  todos: PropTypes.array.isRequired,
+  fetchTodos: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    todos: state.todos,
+  };
+}
+
+const mapDispatchToProps = {
+  fetchTodos,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todo);
