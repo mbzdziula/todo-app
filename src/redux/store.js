e8b35fe87f909abfef3b0837b921039a32b727ef
mbzdziula <mbzdziula@gmail.com>
2021-01-20 16:18:43 +0100

    delete logger and devTools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,7 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
